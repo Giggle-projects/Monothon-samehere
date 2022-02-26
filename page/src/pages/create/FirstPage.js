8682b95828b382a4016f97f36ba23a78ebd8c307
Eugine Park <pkeugine@gmail.com>
2022-02-26 14:09:30 +0900

    refactor: profile image input component 분리

--- page/src/pages/create/FirstPage.js
+++ page/src/pages/create/FirstPage.js
@@ -3,6 +3,27 @@ import Input from "../../components/common/Input";
 import Button from "../../components/common/Button";
 import {ReactComponent as UploadIcon} from "../../static/svg/uploadIcon.svg";
 
+const ProfileImageInput = ({...restProps}) => {
+  return <>
+    <div className="profile-upload">
+      <div className="left">
+        <div className="title">프로필 사진 업로드</div>
+        <div className="description">
+          사진이 없으면 기본 프로필로 등록되어요!
+        </div>
+      </div>
+      <label className="upload-button">
+        <input
+            type="file"
+            accept="image/*"
+            {...restProps}
+        />
+        <UploadIcon/>
+      </label>
+    </div>
+  </>
+}
+
 const FirstPage = (props) => {
 
   const {
@@ -31,24 +52,9 @@ const FirstPage = (props) => {
       </>
     </div>
     <div className="content">
-      <div className="profile-upload">
-        <div className="left">
-          <div className="title">프로필 사진 업로드</div>
-          <div className="description">
-            사진이 없으면 기본 프로필로 등록되어요!
-          </div>
-        </div>
-        <label className="upload-button">
-          <input
-              type="file"
-              accept="image/*"
-              onChange={(e) =>
-                  e.target.files && props.setFile(e.target.files[0])
-              }
-          />
-          <UploadIcon/>
-        </label>
-      </div>
+      <ProfileImageInput 
+        onChange={(e) => e.target.files && props.setFile(e.target.files[0])}
+      />
       <Input
           value={name}
           title="이름"
