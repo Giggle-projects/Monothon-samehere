3d7be5dd7e8b92a4f2a4bc1e18792cf7d58cb80f
ecsimsw <ecsimsw@gmail.com>
2022-03-05 03:31:06 +0900

    feat : resource handler에 utf8 decode resolver 추가

--- server/src/main/java/com/giggle/samehere/config/WebMvcConfig.java
+++ server/src/main/java/com/giggle/samehere/config/WebMvcConfig.java
@@ -1,13 +1,18 @@
 package com.giggle.samehere.config;
 
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolver;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -31,7 +36,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         final Path fileRoot = Paths.get("./" + cardUploadFolderName).toAbsolutePath().normalize();
         registry
-                .addResourceHandler("/"+cardUploadFolderName+"/**")
-                .addResourceLocations(fileRoot.toUri().toString());
+                .addResourceHandler("/" + cardUploadFolderName + "/**")
+                .addResourceLocations(fileRoot.toUri().toString())
+                .resourceChain(true)
+                .addResolver(new Utf8DecodeResourceHandler());
+    }
+}
+
+class Utf8DecodeResourceHandler extends PathResourceResolver implements ResourceResolver {
+
+    @Override
+    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+        resourcePath = URLDecoder.decode(resourcePath, StandardCharsets.UTF_8);
+        return super.getResource(resourcePath, location);
     }
 }
